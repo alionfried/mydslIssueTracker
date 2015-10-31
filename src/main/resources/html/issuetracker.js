87a2b6bf8d6b16291521112d7e48342899581d42
seb <kecksland@web.de>
2015-10-31 19:32:35 +0100

    ch js

--- src/main/resources/html/issuetracker.js
+++ src/main/resources/html/issuetracker.js
@@ -71,7 +71,7 @@ $("#btnCreateNewIssue").click(function () {
         alert("abc");
 	$.get( "http://localhost:4567/getIssuetypes", function( data ) {
   	alert( "Data Loaded: " + data );
-  	var issueTypes = data.issueTypes;  
+  	var issueTypes = data.issueTypes;
 	});
 	
 });
